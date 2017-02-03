7ea024dcd8e63799794e5a5b6b91ee26b59f150d
quazardous <berliozdavid@gmail.com>
2017-02-03 16:44:25 +0100

    land 1.0

---
+++ README.md
@@ -0,0 +1,58 @@
+# Silex ImageStack Demo
+A PHP image serving framework.
+
+The main goal is to provide a robust framework to create an "on the fly" image thumbnailer generator similar to [imagecache](https://www.drupal.org/project/imagecache) / [image style](https://www.drupal.org/docs/8/core/modules/image/working-with-images) in [Drupal](https://www.drupal.org/).
+
+Here is a demo based on [Silex](https://github.com/silexphp/Silex) .
+
+See [SilexImageStack](https://github.com/quazardous/SilexImageStack) for more infos.
+
+## Installation
+
+    git clone git@github.com:quazardous/SilexImageStackDemo.git
+
+Publish the `web` folder on an `apache` server and access it.
+
+## Usage
+
+We can play with [CC0](https://creativecommons.org/publicdomain/zero/1.0/deed.en) images hosted on https://pexels.com/.
+
+I like this one https://static.pexels.com/photos/313094/pexels-photo-313094.jpeg.
+
+Here are some tests you can make with the image stack.
+
+NB: first image request will trigger a download from https://pexels.com/. Then thumbnails will be calculated from cached image.
+
+A second request on the same image will be serve statically.
+
+Original image:
+
+    http://localhost/~you/SilexImageStackDemo/web/img/pexels/original/313094/pexels-photo-313094.jpeg
+
+`big` style image (fit 800x500 box):
+
+    http://localhost/~you/SilexImageStackDemo/web/img/pexels/style/big/313094/pexels-photo-313094.jpeg
+
+`small` style image (crop to 300x200 box):
+
+    http://localhost/~you/SilexImageStackDemo/web/img/pexels/style/small/313094/pexels-photo-313094.jpeg
+
+`thumb` style image (crop to 100x100 box):
+
+    http://localhost/~you/SilexImageStackDemo/web/img/pexels/style/thumb/313094/pexels-photo-313094.jpeg
+    
+Custom format image (use width and height from URL):
+
+    http://localhost/~you/SilexImageStackDemo/web/img/pexels/format/200x300/313094/pexels-photo-313094.jpeg
+
+Other will trigger `404`.
+
+You can purge cached files removing `var/cache/pexels` (cache) and `web\img\pexels` (thumbnails).
+
+Take a look at `app/bootstrap.php` (or here [SilexImageStack](https://github.com/quazardous/SilexImageStack)) to see it done what is done with a few config lines in the **Silex** bootstrap.
+
+## Credits
+[quazardous](https://github.com/quazardous).
+
+## License
+MIT
---
+++ app/bootstrap.php
@@ -0,0 +1,90 @@
+<?php
+date_default_timezone_set('Europe/Paris');
+
+include __DIR__.'/../vendor/autoload.php';
+
+use Silex\Application;
+$app = new Application();
+$app['debug'] = true;
+
+use Sergiors\Silex\Provider\DoctrineCacheServiceProvider;
+$app->register(new DoctrineCacheServiceProvider(), [
+    // default cache
+    'cache.options' => [
+        'driver' => 'raw_file',
+        'root' => __DIR__ . '/../var/cache/pexels',
+    ]
+]);
+
+use ImageStack\Provider\ImageStackProvider;
+$app->register(new ImageStackProvider(), [
+    'image.backends.options' => [
+        // http backend on pexels.com
+        'web' => [
+            'driver' => 'http',
+            'root_url' => 'https://static.pexels.com/photos/',
+        ],
+        // we create a backend that can cache photo from pexels.com locally
+        'web_cache' => [
+            'driver' => 'cache',
+            'backend' => 'web',
+            'cache' => 'default',
+        ],
+        // we create a backend that will rewrite path before fetching from pexels.com (cached)
+        'web_final' => [
+            'driver' => 'path_rule',
+            'backend' => 'web_cache',
+            'rules' => [
+                ['@^((style|format)/[^/]+/)(.*)$@', [3]],
+                ['@^(original/)(.*)$@', [2]],
+            ],
+        ],
+    ],
+    'image.manipulators.options' => [
+        'thumbnails' => [
+            'driver' => 'thumbnailer',
+            'rules' => [
+                ['@^style/big/.*$@', '<800x500'],
+                ['@^style/small/.*$@', '300x200'],
+                ['@^style/thumb/.*$@', '100'],
+                ['@^format/([0-9]+)x([0-9]+)/.*$@', function ($macthes) { return sprintf('%sx%s', $macthes[1], $macthes[2]); }],
+                ['/.*/', false], // trigger a 404
+            ],
+        ],
+    ],
+    'image.storages.options' => [
+        // mount the image on the web bootstrap base folder
+        'img' => [
+            'driver' => 'optimized_file',
+            'root' => __DIR__ . '/../web/img/',
+            'use_prefix' => true,
+            'optimizers' => 'jpeg'
+        ],
+    ],
+    // the stack
+    'image.stacks.options' => [
+        'pexels' => [
+            'backend' => 'web_final',
+            'manipulators' => 'thumbnails',
+            'storage' => 'img',
+        ],
+    ],
+    'image.optimizers.options' => [
+        'jpeg' => 'jpegtran',
+    ],
+]);
+
+use ImageStack\Provider\ImagineProvider;
+$app->register(new ImagineProvider());
+
+$app['session.storage.handler'] = null; // no sessions
+
+use Silex\Provider\ServiceControllerServiceProvider;
+$app->register(new ServiceControllerServiceProvider());
+
+use ImageStack\Provider\ImageControllerProvider;
+$provider = new ImageControllerProvider();
+$app->register($provider);
+$app->mount('/img/', $provider);
+
+return $app;
